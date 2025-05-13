41c5b12b92381d4a827d595454b6e2696cff71bc
SoumyaCodercpp <soumyajitpal2803@gmail.com>
2025-05-13 22:51:32 +0530

    switching to new branch to contribute

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ console.log("LMAO")
 console.log("PRACTICING OPEN-SOURCE")
 
 
-console.log("PRACTICING OPEN-SOURCE after a long time ")
+console.log("PRACTICING OPEN-SOURCE after a long time  ")
+console.log("always create changes in another branch not the main branch , otherwise it will not show the create pull request option")
